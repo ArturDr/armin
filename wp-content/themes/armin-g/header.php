815c696fa2cf403e49f87b22c6319a7eeb50e15b
Artur Robek <robek.artur@gmial.com>
2022-08-19 19:32:02 +0200

    add front-page, realizacje

--- wp-content/themes/armin-g/header.php
+++ wp-content/themes/armin-g/header.php
@@ -26,12 +26,15 @@
 	<?php wp_body_open(); ?>
 	<nav class="navbar navbar-expand-lg bg-light">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="<?php echo site_url();?>">Twoja Nazwa</a>
+			<a class="navbar-brand" href="<?php echo site_url('strona-glowna');?>">Twoja Nazwa</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('/aktualnosci');?>">Aktualności</a>
+					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="<?php echo site_url('/o-nas');?>">O nas</a>
 					</li>
@@ -39,7 +42,7 @@
 						<a class="nav-link" href="<?php echo site_url('/oferta');?>">Oferta</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="<?php echo site_url('/realizacje');?>">Realizacje</a>
+						<a class="nav-link" href="<?php echo get_post_type_archive_link('realizacje');?>">Realizacje</a>
 					</li>
 					<li class="nav-item">
 						<a class="nav-link" href="<?php echo site_url('/kontakt');?>">Kontakt</a>
